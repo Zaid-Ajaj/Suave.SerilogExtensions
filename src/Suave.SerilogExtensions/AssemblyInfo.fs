f9cd0a085b6868364e84fb9b61bf3a073739a602
Zaid <zaid.naom@gmail.com>
2018-03-25 21:03:21 +0200

    Bump version to 0.2.0 * Documentation * Configure request/response/error message templates

--- src/Suave.SerilogExtensions/AssemblyInfo.fs
+++ src/Suave.SerilogExtensions/AssemblyInfo.fs
@@ -4,20 +4,20 @@ open System.Reflection
 
 [<assembly: AssemblyTitleAttribute("Suave.SerilogExtensions")>]
 [<assembly: AssemblyProductAttribute("Suave.SerilogExtensions")>]
-[<assembly: AssemblyVersionAttribute("0.1.0")>]
+[<assembly: AssemblyVersionAttribute("0.2.0")>]
 [<assembly: AssemblyMetadataAttribute("ReleaseDate","2017-03-25T00:00:00.0000000")>]
-[<assembly: AssemblyFileVersionAttribute("0.1.0")>]
-[<assembly: AssemblyInformationalVersionAttribute("0.1.0")>]
+[<assembly: AssemblyFileVersionAttribute("0.2.0")>]
+[<assembly: AssemblyInformationalVersionAttribute("0.2.0")>]
 [<assembly: AssemblyMetadataAttribute("ReleaseChannel","release")>]
-[<assembly: AssemblyMetadataAttribute("GitHash","dd8b365e64feeffaf4f868cba4549538a31c507a")>]
+[<assembly: AssemblyMetadataAttribute("GitHash","1aa41450f152b5bfb720d6314c6d4f87f588c4e7")>]
 do ()
 
 module internal AssemblyVersionInformation =
     let [<Literal>] AssemblyTitle = "Suave.SerilogExtensions"
     let [<Literal>] AssemblyProduct = "Suave.SerilogExtensions"
-    let [<Literal>] AssemblyVersion = "0.1.0"
+    let [<Literal>] AssemblyVersion = "0.2.0"
     let [<Literal>] AssemblyMetadata_ReleaseDate = "2017-03-25T00:00:00.0000000"
-    let [<Literal>] AssemblyFileVersion = "0.1.0"
-    let [<Literal>] AssemblyInformationalVersion = "0.1.0"
+    let [<Literal>] AssemblyFileVersion = "0.2.0"
+    let [<Literal>] AssemblyInformationalVersion = "0.2.0"
     let [<Literal>] AssemblyMetadata_ReleaseChannel = "release"
-    let [<Literal>] AssemblyMetadata_GitHash = "dd8b365e64feeffaf4f868cba4549538a31c507a"
+    let [<Literal>] AssemblyMetadata_GitHash = "1aa41450f152b5bfb720d6314c6d4f87f588c4e7"
